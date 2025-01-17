c4a3d45f5e2492fbdb8b96b2e08b898438d50eb7
Andrew V D <andrewvd31@gmail.com>
2025-01-17 08:55:04 +0530

    Update v1.1

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// Constants Declaration
+//imp! Constants Declaration
 
-const mainBtn = document.querySelector('.main-btn')
-const colorChoose = document.querySelector('.chose-color')
+const mainBtn = document.querySelector('.main-btn');
+const colorChoose = document.querySelector('.chose-color');
 
-// Main Button Event Listener
+//imp! Main Button Event Listener
 
 mainBtn.addEventListener('click',()=>{
-    let rgbStore = ""
+    let rgbStore = "";
     for (let i=0; i<3;i++){
-        // Random number is got from getRandomNumber function and added to rgb store
-        rgbStore += getRandomNumber() + ","
+        //imp! Random number is got from getRandomNumber function and added to rgb store
+        rgbStore += getRandomNumber() + ",";
     }
-    // The last comma is removed using slice function
+    //imp! The last comma is removed using slice function
     rgbStore = rgbStore.slice(0, -1);
-    // The body background color is changed to rgbstore color value and added 
-    // to colorChoose text
-    document.body.style.backgroundColor = `rgb(${rgbStore})`
-    colorChoose.textContent = `rgb(${rgbStore})`
+    //imp! The body background color is changed to rgbstore color value and added 
+    //imp! to colorChoose text
+    document.body.style.backgroundColor = `rgb(${rgbStore})`;
+    colorChoose.textContent = `rgb(${rgbStore})`;
 })
 
 function getRandomNumber(){
-    // Random number is returned here
-    return Math.floor(Math.random()*255)
+    //imp! Random number is returned here
+    return Math.floor(Math.random()*255);
 }
